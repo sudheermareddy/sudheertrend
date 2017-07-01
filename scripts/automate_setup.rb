c713f662d145be23201d0eb0f690f515a29fa6db
sudheermareddy <sudheerm@sysgain.com>
2017-07-01 22:52:32 +0530

    Update automate_setup.rb

--- scripts/automate_setup.rb
+++ scripts/automate_setup.rb
@@ -2,7 +2,7 @@
 # want to eventually convert it into a recipe and have a ctl-command to pass
 # the attributes to chef run when the process for publishing the Azure template
 # has matured.
-
+require 'shellwords'
 require "optparse"
 require "mixlib/shellout"
 require "open-uri"
@@ -10,9 +10,22 @@ require "fileutils"
 
 @license = nil
 @fqdn = nil
-
+@adminUsername = nil
+@firstname = nil 
+@lastname = nil
+@mailid = nil
+@adminpassword = nil
+@orguser = nil
+@fqdnorch = nil
 OptionParser.new do |opts|
   opts.on("--fqdn FQDN", String, "The machine FQDN") { |fqdn| @fqdn = fqdn }
+  opts.on("--adminUsername adminUsername", String, "The machine adminusername") { |adminUsername| @adminUsername = adminUsername }
+  opts.on("--firstname firstname", String, "The machine firstname") { |firstname| @firstname = firstname }
+  opts.on("--lastname lastname", String, "The machine lastname") { |lastname| @lastname = lastname }
+  opts.on("--mailid mailid", String, "The machine mailid") { |mailid| @mailid = mailid }
+  opts.on("--adminpassword adminpassword", String, "The machine adminpassword") { |adminpassword| @adminpassword = adminpassword }
+  opts.on("--orguser orguser", String, "The machine orguser") { |orguser| @orguser = orguser }
+  opts.on("--fqdnorch fqdnorch", String, "The machine fqdnorch") { |fqdnorch| @fqdnorch = fqdnorch }
   opts.on("--license [LICENSE]", "The Automate license file") do |license|
     @license = license
   end
@@ -39,3 +52,47 @@ hostname.run_command
 # Configure Automate
 configure = Mixlib::ShellOut.new("chef-marketplace-ctl setup --preconfigure")
 configure.run_command
+
+sleep 180
+
+
+##Chef-Automate Upgrade
+upgrade = Mixlib::ShellOut.new("chef-marketplace-ctl upgrade -y")
+upgrade.run_command
+
+ reconfigure = Mixlib::ShellOut.new("chef-server-ctl reconfigure")
+ reconfigure.run_command
+
+restartserver = Mixlib::ShellOut.new("chef-server-ctl restart")
+restartserver.run_command
+
+##Creating user for Chef Web UI
+FileUtils.touch('/var/opt/delivery/.telemetry.disabled')
+#exec(sudo touch /var/opt/delivery/.telemetry.disabled)
+creatuser= Mixlib::ShellOut.new("automate-ctl create-user default #{@adminUsername} --password #{@adminpassword}")
+creatuser.run_command
+
+usercreat= Mixlib::ShellOut.new("chef-server-ctl user-create  sdheerms  sudheer ms   password1234 | tee /etc/opscode/sudheerms.pem")
+usercreat.run_command
+
+usercreate= Mixlib::ShellOut.new("chef-server-ctl user-create  #{@adminUsername} #{@firstname} #{@lastname} #{@mailid} #{@adminpassword} | tee /etc/opscode/#{@adminUsername}.pem")
+usercreate.run_command
+
+ orgcreate= Mixlib::ShellOut.new(" chef-server-ctl org-create #{@orguser} NewOrg  -a  #{@adminUsername} | tee /etc/opscode/#{@orguser}-validator.pem")
+ orgcreate.run_command
+
+##pull files from repo
+system("wget 'https://trendmicrop2p.blob.core.windows.net/trendmicropushtopilot/files/validatorkey.txt'  -O /tmp/validatorkey.txt")
+system("wget 'https://trendmicrop2p.blob.core.windows.net/trendmicropushtopilot/files/userkey.txt' -O /tmp/userkey.txt")
+
+##Assigning variable to construct and update key and key-value
+
+validatorke = File.read("/tmp/validatorkey.txt")
+userke = File.read("/tmp/userkey.txt")
+
+#Upload key value.
+FINAL = "\"}' #{@fqdnorch}"
+system("echo #{Shellwords.escape(validatorkey)}`cat /etc/opscode/#{Shellwords.escape(@orguser)}-validator.pem | base64 | tr -d '\n'`#{Shellwords.escape(FINAL)} | bash")
+system("echo #{Shellwords.escape(userkey)}`cat /etc/opscode/#{Shellwords.escape(@adminUsername)}.pem | base64 | tr -d '\n'`#{Shellwords.escape(FINAL)} | bash")
+#echo $validatorkey`cat /etc/opscode/${6}-validator.pem | base64 | tr -d '\n'`$FINAL| bash
+#echo $userkey`cat /etc/opscode/${1}.pem | base64 | tr -d '\n'`$FINAL | bash
